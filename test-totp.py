65f886447224f00cdcceb864944f6fdaf642e775
Jan Varho <jan@varho.org>
2015-08-23 17:01:44 +0300

    Add tests from RFC 6238

--- test-totp.py
+++ test-totp.py
@@ -22,8 +22,7 @@ import totp
 import time
 
 
-class TOTPTests(unittest.TestCase):
-    """Tests TOTP"""
+class TOTPTest(unittest.TestCase):
     def _test_vector(self, secret, at, token, pwd=None, **kwargs):
         t = totp.TOTP(secret, **kwargs)
         if pwd is not None:
@@ -36,6 +35,39 @@ class TOTPTests(unittest.TestCase):
             time.time = tmp
         self.assertEqual(token, o)
 
+
+class RFCVectors(TOTPTest):
+    '''From RFC 6238 Appendix B'''
+    secret1 = '1234567890'*2
+    #For the following cf. http://crypto.stackexchange.com/a/27499/13625
+    secret256 = '1234567890'*3+'12'
+    secret512 = '1234567890'*6+'1234'
+    def _test_all(self, at, t1, t256, t512):
+        self._test_vector(self.secret1, at, t1, h_length=8)
+        self._test_vector(self.secret256, at, t256, h_length=8, h_hash='sha256')
+        self._test_vector(self.secret512, at, t512, h_length=8, h_hash='sha512')
+
+    def test_vectors_1(self):
+        self._test_all(59, '94287082', '46119246', '90693936')
+
+    def test_vectors_2(self):
+        self._test_all(1111111109, '07081804', '68084774', '25091201')
+
+    def test_vectors_3(self):
+        self._test_all(1111111111, '14050471', '67062674', '99943326')
+
+    def test_vectors_4(self):
+        self._test_all(1234567890, '89005924', '91819424', '93441116')
+
+    def test_vectors_5(self):
+        self._test_all(2000000000, '69279037', '90698825', '38618901')
+
+    def test_vectors_6(self):
+        self._test_all(20000000000, '65353130', '77737706', '47863826')
+
+
+class NewVectors(TOTPTest):
+    '''Some random test vectors intended to test all parameters'''
     def test_vector_n1(self):
         self._test_vector('1234', 10, '110366')
 
@@ -87,5 +119,6 @@ class TOTPTests(unittest.TestCase):
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TOTPTests))
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(RFCVectors))
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(NewVectors))
     unittest.TextTestRunner().run(suite)
