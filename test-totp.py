56c481b2c8824368ae873ef2f56457b1d4f292b6
Jan Varho <jan@varho.org>
2015-09-03 15:02:04 +0300

    Make unit tests work with Py3

--- test-totp.py
+++ test-totp.py
@@ -38,83 +38,83 @@ class TOTPTest(unittest.TestCase):
 
 class RFCVectors(TOTPTest):
     '''From RFC 6238 Appendix B'''
-    secret1 = '1234567890'*2
+    secret1 = b'1234567890'*2
     #For the following cf. http://crypto.stackexchange.com/a/27499/13625
-    secret256 = '1234567890'*3+'12'
-    secret512 = '1234567890'*6+'1234'
+    secret256 = b'1234567890'*3+b'12'
+    secret512 = b'1234567890'*6+b'1234'
     def _test_all(self, at, t1, t256, t512):
         self._test_vector(self.secret1, at, t1, h_length=8)
         self._test_vector(self.secret256, at, t256, h_length=8, h_hash='sha256')
         self._test_vector(self.secret512, at, t512, h_length=8, h_hash='sha512')
 
     def test_vectors_1(self):
-        self._test_all(59, '94287082', '46119246', '90693936')
+        self._test_all(59, b'94287082', b'46119246', b'90693936')
 
     def test_vectors_2(self):
-        self._test_all(1111111109, '07081804', '68084774', '25091201')
+        self._test_all(1111111109, b'07081804', b'68084774', b'25091201')
 
     def test_vectors_3(self):
-        self._test_all(1111111111, '14050471', '67062674', '99943326')
+        self._test_all(1111111111, b'14050471', b'67062674', b'99943326')
 
     def test_vectors_4(self):
-        self._test_all(1234567890, '89005924', '91819424', '93441116')
+        self._test_all(1234567890, b'89005924', b'91819424', b'93441116')
 
     def test_vectors_5(self):
-        self._test_all(2000000000, '69279037', '90698825', '38618901')
+        self._test_all(2000000000, b'69279037', b'90698825', b'38618901')
 
     def test_vectors_6(self):
-        self._test_all(20000000000, '65353130', '77737706', '47863826')
+        self._test_all(20000000000, b'65353130', b'77737706', b'47863826')
 
 
 class NewVectors(TOTPTest):
     '''Some random test vectors intended to test all parameters'''
     def test_vector_n1(self):
-        self._test_vector('1234', 10, '110366')
+        self._test_vector(b'1234', 10, b'110366')
 
     def test_vector_n2(self):
-        self._test_vector('1234', 20, '110366')
+        self._test_vector(b'1234', 20, b'110366')
 
     def test_vector_n3(self):
-        self._test_vector('1234', 40, '336582')
+        self._test_vector(b'1234', 40, b'336582')
 
     def test_vector_n4(self):
-        self._test_vector('1234', 10, '8110366', h_length=7)
+        self._test_vector(b'1234', 10, b'8110366', h_length=7)
 
     def test_vector_n5(self):
-        self._test_vector('1234', 10, '18110366', h_length=8)
+        self._test_vector(b'1234', 10, b'18110366', h_length=8)
 
     def test_vector_n6(self):
-        self._test_vector('1234', 10, '127174', h_hash='sha256')
+        self._test_vector(b'1234', 10, b'127174', h_hash='sha256')
 
     def test_vector_n7(self):
-        self._test_vector('1234', 10, '637043', h_hash='sha512')
+        self._test_vector(b'1234', 10, b'637043', h_hash='sha512')
 
     def test_vector_n8(self):
-        self._test_vector('1234', 100, '110366', t_zero=90)
+        self._test_vector(b'1234', 100, b'110366', t_zero=90)
 
     def test_vector_n9(self):
-        self._test_vector('1234', 40, '110366', t_timeout=60)
+        self._test_vector(b'1234', 40, b'110366', t_timeout=60)
 
     def test_vector_n10(self):
-        self._test_vector('1234', 100, '336582', t_timeout=60)
+        self._test_vector(b'1234', 100, b'336582', t_timeout=60)
 
     def test_vector_p1(self):
-        self._test_vector('asdf', 10, '036575', pwd='qwerty', salt='1234')
+        self._test_vector(b'asdf', 10, b'036575', pwd=b'qwerty', salt=b'1234')
 
     def test_vector_p2(self):
-        self._test_vector('asdf', 20, '036575', pwd='qwerty', salt='1234')
+        self._test_vector(b'asdf', 20, b'036575', pwd=b'qwerty', salt=b'1234')
 
     def test_vector_p3(self):
-        self._test_vector('asdf', 40, '865509', pwd='qwerty', salt='1234')
+        self._test_vector(b'asdf', 40, b'865509', pwd=b'qwerty', salt=b'1234')
 
     def test_vector_p4(self):
-        self._test_vector('asdfg', 10, '015311', pwd='qwerty', salt='1234')
+        self._test_vector(b'asdfg', 10, b'015311', pwd=b'qwerty', salt=b'1234')
 
     def test_vector_p5(self):
-        self._test_vector('asdf', 10, '986141', pwd='qwertyu', salt='1234')
+        self._test_vector(b'asdf', 10, b'986141', pwd=b'qwertyu', salt=b'1234')
 
     def test_vector_p6(self):
-        self._test_vector('asdf', 10, '490784', pwd='qwerty', salt='12345')
+        self._test_vector(b'asdf', 10, b'490784', pwd=b'qwerty', salt=b'12345')
 
 
 if __name__ == '__main__':
